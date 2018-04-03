4bd376782319783223d119f4f66de3c64c28069c
maxzerrrrrr <34354306+maxzerrrrrr@users.noreply.github.com>
2018-04-03 16:07:36 +0200

    Modification fichier header

--- ECEBio/Arcs.h
+++ ECEBio/Arcs.h
@@ -1,6 +1,44 @@
 #ifndef ARCS_H_INCLUDED
 #define ARCS_H_INCLUDED
 
+class Arcs
+{
+public:
 
+        ///constructor & destructor
+        arrete();
+        ~arrete();
+
+        ///getter
+        gets1();
+        gets2();
+        getspoid();
+        getactf();
+        getselec();
+
+        ///setter
+        sets1();
+        sets2();
+        setpoid();
+        setactf();
+        setselec();
+
+        ///fonction
+        arreteactive(actif);
+        arreteselectionner(selectionner);
+        allocmeme();
+
+    protected:
+
+        ///sommets
+        int * m_s1;
+        int * m_s2;
+
+        int m_poids;
+
+        ///booléen
+        bool actif;
+        bool selectionner;
 
 #endif // ARCS_H_INCLUDED
+}
